da3d9c21e6096dddb332435f11b24f29e10cac6f
Rich Trott <rtrott@gmail.com>
2020-02-26 18:08:15 -0800

    add Mc

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ var sample = require('lodash.sample');
 module.exports = function () {
 	var givenName = sample(words.givenPart1) + sample(words.givenPart2);
 	var surname = sample(words.surnamePart1) + sample(words.surnamePart2);
+	// 1% chance of prefix with "Mc"
+	if (Math.random() < 0.1) {
+		surname = 'Mc' + surname;
+	}
 	return givenName + ' ' + surname;
 };
