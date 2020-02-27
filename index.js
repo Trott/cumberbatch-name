1cbdd67a1989e854b18e8f1d382c3f09538488f7
Rich Trott <rtrott@gmail.com>
2020-02-26 19:58:39 -0800

    add Von

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var sample = require('lodash.sample');
 module.exports = function () {
 	var givenName = sample(words.givenPart1) + sample(words.givenPart2);
 	var surname = sample(words.surnamePart1) + sample(words.surnamePart2);
+	// 1% chance of prefix with "Von"
+	if (Math.random() < 0.01) {
+		surname = 'Von ' + surname;
+	}
 	// 1% chance of prefix with "Mc"
 	if (Math.random() < 0.01) {
 		surname = 'Mc' + surname;
