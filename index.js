b671be19c6544a91d1262f248b8e84574f2d5715
Rich Trott <rtrott@gmail.com>
2020-10-14 21:14:27 -0700

    fix: fix Mc + Von edge case
    Adding 100% test coverage revealed a rare bug where names would be
    Baloneydock McVon Sundayfries instead of Baloneydock Von McSundayfries.

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var sample = require('lodash.sample')
 module.exports = function () {
   var givenName = sample(words.givenPart1) + sample(words.givenPart2)
   var surname = sample(words.surnamePart1) + sample(words.surnamePart2)
-  // 1% chance of prefix with "Von"
-  if (Math.random() < 0.01) {
-    surname = 'Von ' + surname
-  }
   // 1% chance of prefix with "Mc"
   if (Math.random() < 0.01) {
     surname = 'Mc' + surname
+  }
+  // 1% chance of prefix with "Von"
+  if (Math.random() < 0.01) {
+    surname = 'Von ' + surname
   }
   return givenName + ' ' + surname
 }
--- test.js
+++ test.js
@@ -7,5 +7,15 @@ test('returns a random Cumberbatch name', function (t) {
 })
 
 test('returns a given name and a surname', function (t) {
+  t.assert(cumberbatch().match(/^\w+ (?:Von )?\w+$/))
+})
+
+test('uses "Von" and "Mc" infrequently', function (t) {
+  var original = Math.random
+  Math.random = () => 0
+  var name = cumberbatch()
+  t.assert(name.match(/^\w+ Von Mc\w+$/), name)
+  Math.random = () => 0.1
   t.assert(cumberbatch().match(/^\w+ \w+$/))
+  Math.random = original
 })
