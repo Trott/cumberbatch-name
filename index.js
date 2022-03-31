bca572c2b75ef69bedeb99d1bc559e04ef1ebb6f
Rich Trott <rtrott@gmail.com>
2022-03-31 16:44:52 +0000

    chore: update development dependencies

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict'
-var words = require('./words.json')
-var sample = require('lodash.sample')
+const words = require('./words.json')
+const sample = require('lodash.sample')
 
 module.exports = function () {
-  var givenName = sample(words.givenPart1) + sample(words.givenPart2)
-  var surname = sample(words.surnamePart1) + sample(words.surnamePart2)
+  const givenName = sample(words.givenPart1) + sample(words.givenPart2)
+  let surname = sample(words.surnamePart1) + sample(words.surnamePart2)
   // 1% chance of prefix with "Mc"
   if (Math.random() < 0.01) {
     surname = 'Mc' + surname
