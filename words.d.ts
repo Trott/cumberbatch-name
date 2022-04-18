5b68a17c26936ec64e7d2a53cacc91ac88f9a3fc
Rich Trott <rtrott@gmail.com>
2022-04-18 05:16:11 -0700

    feat: add TypeScript types
    BREAKING CHANGE: module is now ESM-only
    BREAKING CHANGE: remove -h/--help options from CLI

---
+++ words.d.ts
@@ -0,0 +1,8 @@
+declare const _default: {
+    givenPart1: string[];
+    givenPart2: string[];
+    surnamePart1: string[];
+    surnamePart2: string[];
+};
+export default _default;
+//# sourceMappingURL=words.d.ts.map
